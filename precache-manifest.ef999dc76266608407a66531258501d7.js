1eaa65aea6197a31124af8e4d02ec0fc91c03be0
Daniel Bedggood <ddbedggood@gmail.com>
2018-12-31 16:24:30 +1300

    Updates

---
+++ precache-manifest.ef999dc76266608407a66531258501d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d207dc04e256e6153b6f8de229c1394",
+    "url": "/connect4/static/media/c4y.7d207dc0.png"
+  },
+  {
+    "revision": "90fdc2044dd333a68a0e6444a3e3b37c",
+    "url": "/connect4/static/media/c4r.90fdc204.png"
+  },
+  {
+    "revision": "d141bee2fdd22c92f4e4",
+    "url": "/connect4/static/js/runtime~main.d141bee2.js"
+  },
+  {
+    "revision": "17bd64fa6f51bebec344",
+    "url": "/connect4/static/js/main.17bd64fa.chunk.js"
+  },
+  {
+    "revision": "ac9ae1edcdde807d9986",
+    "url": "/connect4/static/js/1.ac9ae1ed.chunk.js"
+  },
+  {
+    "revision": "17bd64fa6f51bebec344",
+    "url": "/connect4/static/css/main.93d5da84.chunk.css"
+  },
+  {
+    "revision": "211fe8e1b8df7ec91e832028c43b4d89",
+    "url": "/connect4/index.html"
+  }
+];
