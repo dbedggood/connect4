d20b736b32e1e6366b493372dac2331c5ebefb78
Daniel Bedggood <ddbedggood@gmail.com>
2019-01-28 08:37:33 +1300

    Refactor calcWinner.js

--- src/functions/calcWinner.js
+++ src/functions/calcWinner.js
@@ -1,79 +1,56 @@
 function calcWinner(game, lastMove) {
 
-    const colour = lastMove[0]
-    const column = lastMove[1] + 3
-    const row = lastMove[2] + 3
+  const colour = lastMove[0]
+  const column = lastMove[1] + 3
+  const row = lastMove[2] + 3
 
-    let borderedGame = game.slice()
-    const gap = [null, null, null]
-    
-    for (let i=0; i < borderedGame.length; i++) {
-      borderedGame[i] = gap.concat(borderedGame[i]).concat(gap)
+  let borderedGame = game.slice()
+  const gap = [null, null, null]
+
+  for (let i = 0; i < borderedGame.length; i++) {
+    borderedGame[i] = gap.concat(borderedGame[i]).concat(gap)
+  }
+
+  const border = new Array(borderedGame[0].length).fill(null)
+  borderedGame.push(border, border, border)
+  borderedGame.unshift(border, border, border)
+
+  let leftToRight = 0
+  let topToBottom = 0
+  let bottomLeftToTopRight = 0
+  let topLeftToBottomRight = 0
+
+  for (let i = -3; i < 4; i++) {
+    if (borderedGame[column + i][row] === colour) {
+      leftToRight++
+      if (leftToRight >= 4) {return colour}
+    } else {
+      leftToRight = 0
     }
 
-    const border = new Array(borderedGame[0].length).fill(null)
-    borderedGame.push(border, border, border)
-    borderedGame.unshift(border, border, border)
-
-    let count = 0
-
-    // Horizontal Win Check
-    for (let i = -3; i < 4; i++) {
-      if (borderedGame[column + i][row] === colour) {
-        count++
-
-        if (count >=4) {
-          return colour
-        }
-
-      } else {
-        count = 0
-      }
+    if (borderedGame[column][row + i] === colour) {
+      topToBottom++
+      if (topToBottom >= 4) {return colour}
+    } else {
+      topToBottom = 0
     }
 
-    // Vertical Win Check
-    for (let i = -3; i < 4; i++) {
-      if (borderedGame[column][row + i] === colour) {
-        count++
-
-        if (count >=4) {
-            return colour
-        }
-
-      } else {
-        count = 0
-      }
+    if (borderedGame[column + i][row + i] === colour) {
+      bottomLeftToTopRight++
+      if (bottomLeftToTopRight >= 4) {return colour}
+    } else {
+      bottomLeftToTopRight = 0
     }
 
-    // Diagonal (Bottom Left -> Top Right) Win Check
-    for (let i = -3; i < 4; i++) {
-      if (borderedGame[column + i][row + i] === colour) {
-        count++
-
-        if (count >=4) {
-            return colour
-        }
-
-      } else {
-        count = 0
-      }
+    if (borderedGame[column + i][row - i] === colour) {
+      topLeftToBottomRight++
+      if (topLeftToBottomRight >= 4) {return colour}
+    } else {
+      topLeftToBottomRight = 0
     }
+  }
 
-    // Diagonal (Top Left -> Bottom Right) Win Check
-    for (let i = -3; i < 4; i++) {
-      if (borderedGame[column + i][row - i] === colour) {
-        count++
-
-        if (count >=4) {
-          return colour
-        }
-
-      } else {
-        count = 0
-      }
-    }
-
-    return null
+  return null
 
 }
 
